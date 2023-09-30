e9714d2595e275eea0ffe041c1ac97bc0ab32947
Vincent <thisoneis4business@gmail.com>
2023-09-30 16:28:28 +1300

    feat: presets

--- lib/game.ts
+++ lib/game.ts
@@ -4,385 +4,385 @@ export const all = [
     icon: "🥣",
     category: "Utensil",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "spoon",
     icon: "🥄",
     category: "Utensil",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "knife",
     icon: "🔪",
     category: "Utensil",
     difficulty: 3,
-  },
+  } as const,
   {
     name: "cup",
     icon: "🥤",
     category: "Beverage",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "wine glass",
     icon: "🍷",
     category: "Beverage",
     difficulty: 4,
-  },
+  } as const,
   {
     name: "bottle",
     icon: "🍾",
     category: "Beverage",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "cake",
     icon: "🎂",
     category: "Food",
     difficulty: 3,
-  },
+  } as const,
   {
     name: "donut",
     icon: "🍩",
     category: "Food",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "pizza",
     icon: "🍕",
     category: "Food",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "hot dog",
     icon: "🌭",
     category: "Food",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "sandwich",
     icon: "🥪",
     category: "Food",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "carrot",
     icon: "🥕",
     category: "Groceries",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "broccoli",
     icon: "🥦",
     category: "Groceries",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "orange",
     icon: "🍊",
     category: "Groceries",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "apple",
     icon: "🍎",
     category: "Groceries",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "banana",
     icon: "🍌",
     category: "Groceries",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "surfboard",
     icon: "🏄‍♂️",
     category: "Sport Gear",
     difficulty: 6,
-  },
+  } as const,
   {
     name: "skateboard",
     icon: "🛹",
     category: "Sport Gear",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "snowboard",
     icon: "🏂",
     category: "Sport Gear",
     difficulty: 6,
-  },
+  } as const,
   {
     name: "skis",
     icon: "🎿",
     category: "Sport Gear",
     difficulty: 4,
-  },
+  } as const,
   {
     name: "kite",
     icon: "🪁",
     category: "Sport Object",
     difficulty: 7,
-  },
+  } as const,
   {
     name: "sports ball",
     icon: "⚽",
     category: "Sport Object",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "frisbee",
     icon: "🥏",
     category: "Sport Object",
     difficulty: 3,
-  },
+  } as const,
   {
     name: "phone",
     icon: "📱",
     category: "Electronics",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "keyboard",
     icon: "⌨️",
     category: "Electronics",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "mouse",
     icon: "🖱️",
     category: "Electronics",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "laptop",
     icon: "💻",
     category: "Electronics",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "tvmonitor",
     icon: "📺",
     category: "Electronics",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "toothbrush",
     icon: "🪥",
     category: "Small Object",
     difficulty: 3,
-  },
+  } as const,
   {
     name: "teddy bear",
     icon: "🧸",
     category: "Small Object",
     difficulty: 5,
-  },
+  } as const,
   {
     name: "scissors",
     icon: "✂️",
     category: "Small Object",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "clock",
     icon: "🕰️",
     category: "Small Object",
     difficulty: 3,
-  },
+  } as const,
   {
     name: "book",
     icon: "📚",
     category: "Small Object",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "chair",
     icon: "🪑",
     category: "Public Object",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "traffic light",
     icon: "🚦",
     category: "Public Object",
     difficulty: 5,
-  },
+  } as const,
   {
     name: "vase",
     icon: "🏺",
     category: "House Object",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "pottedplant",
     icon: "🪴",
     category: "House Object",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "sofa",
     icon: "🛋️",
     category: "House Object",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "toilet",
     icon: "🚽",
     category: "House Object",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "bed",
     icon: "🛏️",
     category: "House Object",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "suitcase",
     icon: "💼",
     category: "Clothing Object",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "umbrella",
     icon: "☂️",
     category: "Clothing Object",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "tie",
     icon: "👔",
     category: "Clothing Object",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "handbag",
     icon: "👜",
     category: "Clothing Object",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "backpack",
     icon: "🎒",
     category: "Clothing Object",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "boat",
     icon: "⛵",
     category: "Public Transport",
     difficulty: 10,
-  },
+  } as const,
   {
     name: "train",
     icon: "🚂",
     category: "Public Transport",
     difficulty: 10,
-  },
+  } as const,
   {
     name: "bus",
     icon: "🚌",
     category: "Public Transport",
     difficulty: 3,
-  },
+  } as const,
   {
     name: "aeroplane",
     icon: "🛩️",
     category: "Public Transport",
     difficulty: 10,
-  },
+  } as const,
   {
     name: "truck",
     icon: "🚛",
     category: "Transport",
     difficulty: 5,
-  },
+  } as const,
   {
     name: "motorbike",
     icon: "🛵",
     category: "Transport",
     difficulty: 6,
-  },
+  } as const,
   {
     name: "car",
     icon: "🚘",
     category: "Transport",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "bicycle",
     icon: "🚲",
     category: "Transport",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "giraffe",
     icon: "🦒",
     category: "Wild Entity",
     difficulty: 10,
-  },
+  } as const,
   {
     name: "zebra",
     icon: "🦓",
     category: "Wild Entity",
     difficulty: 10,
-  },
+  } as const,
   {
     name: "bear",
     icon: "🐻",
     category: "Wild Entity",
     difficulty: 9,
-  },
+  } as const,
   {
     name: "elephant",
     icon: "🐘",
     category: "Wild Entity",
     difficulty: 10,
-  },
+  } as const,
   {
     name: "cow",
     icon: "🐮",
     category: "Farm Entity",
     difficulty: 7,
-  },
+  } as const,
   {
     name: "sheep",
     icon: "🐑",
     category: "Farm Entity",
     difficulty: 7,
-  },
+  } as const,
   {
     name: "horse",
     icon: "🐴",
     category: "Farm Entity",
     difficulty: 8,
-  },
+  } as const,
   {
     name: "person",
     icon: "👤",
     category: "House Entity",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "bird",
     icon: "🕊️",
     category: "House Entity",
     difficulty: 2,
-  },
+  } as const,
   {
     name: "cat",
     icon: "🐱",
     category: "House Entity",
     difficulty: 1,
-  },
+  } as const,
   {
     name: "dog",
     icon: "🐶",
     category: "House Entity",
     difficulty: 1,
-  },
+  } as const,
 ];
 
 export const categories = all.reduce((acc, curr) => {
@@ -400,22 +400,142 @@ export const difficulties = [
   ["hardest", [20, 26]],
 ] as const;
 
-export function random() {
-  const count = Math.floor(Math.random() * 3) + 2;
-  const items = Array.from({ length: count }, () => false).map(
-    () => all[Math.floor(Math.random() * all.length)]
-  );
+export function item(name: (typeof all)[number]["name"]): (typeof all)[number] {
+  return all.find((item) => item.name === name)!;
+}
 
+export function randomItem() {
+  const weights = all
+    .map((item) => 11 - item.difficulty)
+    .reduce((acc, x) => acc + x, 0);
+
+  let cost = Math.floor(Math.random() * weights);
+
+  for (const item of all) {
+    cost -= 11 - item.difficulty;
+    if (cost <= 0) {
+      return item;
+    }
+  }
+
+  return all[Math.floor(Math.random() * all.length)];
+}
+
+export function difficulty(items: typeof all) {
   const difficulty = items.reduce(
     (acc, curr) => acc + curr.difficulty,
     items.length
   );
   for (const [key, [min, max]] of difficulties) {
     if (difficulty >= min && difficulty <= max) {
-      return { items, difficulty: key };
+      return key;
+    }
+  }
+  return undefined;
+}
+
+export type Preset = {
+  name: string;
+  get: () => {
+    items: typeof all;
+    difficulty: (typeof difficulties)[number][0];
+  };
+  weight: number;
+};
+
+export const presets = [
+  {
+    name: "Go for groceries",
+    get() {
+      const groceries = [
+        item("carrot"),
+        item("apple"),
+        item("banana"),
+        item("orange"),
+        item("broccoli"),
+      ];
+      const count = Math.floor(Math.random() * 3) + 2;
+      const items = Array.from({ length: count }, () => false).map(
+        () => groceries[Math.floor(Math.random() * groceries.length)]
+      );
+      const diff = difficulty(items);
+
+      if (!diff) {
+        return this.get();
+      }
+
+      return {
+        items,
+        difficulty: diff,
+      };
+    },
+    weight: 3,
+  },
+  {
+    name: "Get social",
+    get() {
+      const count = Math.floor(Math.random() * 3) + 2;
+      const items = Array.from({ length: count }, () => false).map(() =>
+        item("person")
+      );
+      return { items, difficulty: difficulty(items) ?? "hardest" };
+    },
+    weight: 5,
+  },
+  {
+    name: "Selfie with",
+    get() {
+      const count = Math.floor(Math.random() * 3) + 1;
+      const items = [
+        item("person"),
+        ...Array.from({ length: count }, () => false).map(() => randomItem()),
+      ];
+      const diff = difficulty(items);
+      if (!diff) {
+        return this.get();
+      }
+      return { items, difficulty: diff };
+    },
+    weight: 10,
+  },
+  {
+    name: "Catch a plane",
+    get() {
+      const items = [item("aeroplane"), item("person")];
+      return { items, difficulty: difficulty(items) ?? "hardest" };
+    },
+    weight: 1,
+  },
+  {
+    name: "Random",
+    get() {
+      const count = Math.floor(Math.random() * 3) + 2;
+      const items = Array.from({ length: count }, () => false).map(() =>
+        randomItem()
+      );
+      const diff = difficulty(items);
+      if (!diff) {
+        return this.get();
+      }
+      return { items, difficulty: diff };
+    },
+    weight: 2,
+  },
+] satisfies Preset[];
+
+export function preset() {
+  const weights = presets
+    .map(({ weight }) => weight)
+    .reduce((acc, x) => acc + x, 0);
+
+  let cost = Math.floor(Math.random() * weights);
+
+  for (const preset of presets) {
+    cost -= preset.weight;
+    if (cost <= 0) {
+      return preset;
     }
   }
 
-  // Too hard, try again
-  return random();
+  return presets[Math.floor(Math.random() * presets.length)];
 }
--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 import { router } from "https://deno.land/x/rutt@0.1.0/mod.ts";
 import { json } from "./lib/cors.ts";
-import { all, random } from "./lib/game.ts";
+import { Preset, all, preset } from "./lib/game.ts";
 import { kv } from "./lib/kv.ts";
 
 Deno.serve(
@@ -9,16 +9,26 @@ Deno.serve(
     "/oftheday": async (req) => {
       const date = new Date();
       const day = date.toISOString().split("T")[0];
-      const entry = await kv.get<ReturnType<typeof random>>(["daily", day]);
+      const entry = await kv.get<ReturnType<Preset["get"]>>(["daily", day]);
       if (entry.value) {
         return json(req, entry.value);
       }
-      const value = random();
+      const curr = preset();
+
+      console.log(`${curr.name}`);
+
+      const value = curr.get();
       await kv.set(["daily", day], { ...value, day });
       return json(req, value);
     },
     "/lib": (req) => {
       return json(req, all);
+    },
+    "/random": (req) => {
+      const curr = preset();
+      console.log(`${curr.name}`);
+      const value = curr.get();
+      return json(req, value);
     },
   })
 );
